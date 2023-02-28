34fc7f24d692102195ed93c160d84b7786d68a9d
Nora <89257225+600891@users.noreply.github.com>
2023-02-28 13:47:01 +0100

    Add files via upload

---
+++ Uno/src/main/java/no/hvl/dat109/Uno/UnoApplication.java
@@ -0,0 +1,13 @@
+package no.hvl.dat109.Uno;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnoApplication.class, args);
+	}
+
+}
---
+++ Uno/src/test/java/no/hvl/dat109/Uno/UnoApplicationTests.java
@@ -0,0 +1,13 @@
+package no.hvl.dat109.Uno;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
