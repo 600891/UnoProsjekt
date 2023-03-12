bd667c05fde961c3cb77c754829c8e448b345fd1
nora <600891@stud.hvl.no>
2023-03-12 22:30:09 +0100

    Example publishing branch

--- Uno/src/main/java/no/hvl/dat109/Uno/UnoApplication.java
+++ Uno/src/main/java/no/hvl/dat109/Uno/UnoApplication.java
@@ -2,9 +2,17 @@ package no.hvl.dat109.Uno;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class UnoApplication {
+public class UnoApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application)
+	{
+		return application.sources(UnoApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(UnoApplication.class, args);
