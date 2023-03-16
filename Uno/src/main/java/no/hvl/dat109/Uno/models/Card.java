c35fca8ddb54afe15fa91ec2e31716b615a3bff0
SiriSlyk <siri.slyk@gmail.com>
2023-03-16 20:46:28 +0100

    Card class done and tested

--- Uno/src/main/java/no/hvl/dat109/Uno/models/Card.java
+++ Uno/src/main/java/no/hvl/dat109/Uno/models/Card.java
@@ -1,10 +1,27 @@
 package no.hvl.dat109.Uno.models;
 
+import jakarta.persistence.Entity;
+
+/**
+ *
+ */
+@Entity
 public class Card {
 
     Color color;
 
+    /**
+     * @param color
+     */
     public Card(Color color) {
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
         this.color = color;
     }
 }
