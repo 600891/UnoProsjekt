7efcc1ff33837055855efa02a6573f3b53d522a6
NoraKri <106915799+NoraKri@users.noreply.github.com>
2023-03-16 16:10:03 +0100

    install sockjs and stompjs, add component folder.

--- UnoFrontend/react/src/component/Uno.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Uno = () => {
-  return <div></div>;
-};
-
-export default Uno;
--- UnoFrontend/react/src/index.js
+++ UnoFrontend/react/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import HelloWorld from "./component/HelloWorld";
+import HelloWorld from "./HelloWorld";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
