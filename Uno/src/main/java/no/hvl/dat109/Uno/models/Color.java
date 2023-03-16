e23d0150a4b25e98d39952f08971d65a68cccfd0
SiriSlyk <siri.slyk@gmail.com>
2023-03-16 20:37:58 +0100

    Making Color-enum class

--- Uno/src/main/java/no/hvl/dat109/Uno/models/Color.java
+++ Uno/src/main/java/no/hvl/dat109/Uno/models/Color.java
@@ -1,6 +1,24 @@
 package no.hvl.dat109.Uno.models;
 
+/**
+ *  Colors that can be used in a card
+ */
 public enum Color {
 
-    BLUE, GREEN, RED, YELLOW
+    /**
+     * Blue color
+     */
+    BLUE,
+    /**
+     * Green color
+     */
+    GREEN,
+    /**
+     * Red color
+     */
+    RED,
+    /**
+     * Yellow color
+     */
+    YELLOW
 }
