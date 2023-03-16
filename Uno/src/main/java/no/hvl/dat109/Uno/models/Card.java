cbc21b96bb117bb46bc5083a873ef70f29b5d042
SiriSlyk <siri.slyk@gmail.com>
2023-03-16 11:28:24 +0100

    Making Card-entity and Color-enum

--- Uno/src/main/java/no/hvl/dat109/Uno/models/Card.java
+++ Uno/src/main/java/no/hvl/dat109/Uno/models/Card.java
@@ -2,5 +2,9 @@ package no.hvl.dat109.Uno.models;
 
 public class Card {
 
+    Color color;
 
+    public Card(Color color) {
+        this.color = color;
+    }
 }
---
+++ Uno/src/main/java/no/hvl/dat109/Uno/models/Color.java
@@ -0,0 +1,6 @@
+package no.hvl.dat109.Uno.models;
+
+public enum Color {
+
+    BLUE, GREEN, RED, YELLOW
+}
