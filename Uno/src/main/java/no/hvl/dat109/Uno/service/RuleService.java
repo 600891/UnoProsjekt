14369280523fc6dfe05d09dd8ab0df5d0aa92ef3
Oda Bastesen Storebo <599030@stud.hvl.no>
2023-03-20 11:03:28 +0100

    Fixed CardRulesTest.

--- Uno/src/main/java/no/hvl/dat109/Uno/service/RuleService.java
+++ Uno/src/main/java/no/hvl/dat109/Uno/service/RuleService.java
@@ -4,7 +4,7 @@ import no.hvl.dat109.Uno.models.Card;
 import org.springframework.stereotype.Service;
 
 @Service
-public class RuleService {
+public class CardRuleService {
 
     /**
      * Checks if the card a player wants to play is playable.
@@ -12,7 +12,7 @@ public class RuleService {
      * @param cardOnTable the card at the top of the card stack of played cards on the table
      * @return true if card is playable and false if card is not playable
      */
-    public boolean isCardPlayable(Card cardToPlay, Card cardOnTable) {
+    public static boolean isCardPlayable(Card cardToPlay, Card cardOnTable) {
         return cardToPlay.getColor().equals(cardOnTable.getColor());
     }
 
--- Uno/src/test/java/no/hvl/dat109/Uno/models/CardRulesTest.java
+++ Uno/src/test/java/no/hvl/dat109/Uno/models/CardRulesTest.java
@@ -1,5 +1,6 @@
 package no.hvl.dat109.Uno.models;
 
+import no.hvl.dat109.Uno.service.RuleService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -14,11 +15,11 @@ public class CardRulesTest {
      * @return
      */
     @Test
-    boolean isCardLegal(){
+    void isCardLegal(){
         Card playerCardRed = new Card(Color.RED);
         Card playerCardBlue = new Card(Color.BLUE);
         Card deckCardBlue = new Card(Color.BLUE);
-        Assertions.assertTrue(CardRules.isCardLegal(playerCardBlue, deckCardBlue));
-        Assertions.assertFalse(CardRules.isCardLegal(playerCardRed, deckCardBlue));
+        Assertions.assertTrue(CardRuleService.isCardPlayable(playerCardBlue, deckCardBlue));
+        Assertions.assertFalse(CardRuleService.isCardPlayable(playerCardRed, deckCardBlue));
     }
 }
