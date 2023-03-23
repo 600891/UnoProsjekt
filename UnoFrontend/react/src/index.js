72bab74e3d44f3c8fddbe1b555e2dec816b991c3
NoraKri <106915799+NoraKri@users.noreply.github.com>
2023-03-23 14:37:36 +0100

    some refactoring

--- UnoFrontend/react/src/index.js
+++ UnoFrontend/react/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { AuthProvider } from "./auth/AuthProvider";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </Router>
   </React.StrictMode>
 );
