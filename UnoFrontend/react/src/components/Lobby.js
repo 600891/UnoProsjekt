66cfc39ee4479516884c82288772c3d65c3ac2f2
Stian <89031614+h598993@users.noreply.github.com>
2023-03-27 20:12:59 +0200

    Added new component create user. Also added styles to log in, lobby and create user screen

--- UnoFrontend/react/src/components/Lobby.js
+++ UnoFrontend/react/src/components/Lobby.js
@@ -8,18 +8,31 @@ function Lobby() {
   const returnToLogin = () => {
     navigate("/", { replace: true });
   };
+  // must fetch data to be displayed in the lobby and make sure the user is logged in
+  const fetchData = () => {};
   const joinGame = () => {
     navigate("/gameroom", { replace: true });
   };
+  // will create a new game session
+  const handleCreateSession = () => {};
 
   return (
-    <div>
-      <h1>
-        Welcome to the lobby {location.state ? location.state.userName : ""}{" "}
-      </h1>
+    <div className="background-image">
+      <div className="parent">
+        <div className="center">
+          <h1 className="white-text h1">Welcome to the lobby</h1>
 
-      <button onClick={joinGame}>Join UNO-Session</button>
-      <button onClick={returnToLogin}>Logout</button>
+          <button className="button" onClick={joinGame}>
+            Join UNO-Session
+          </button>
+          <button className="button" onClick={handleCreateSession}>
+            Create new game session
+          </button>
+          <button className="button" onClick={returnToLogin}>
+            Logout
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
