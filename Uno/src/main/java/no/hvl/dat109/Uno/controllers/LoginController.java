131f78bd1eccca6cc0839597d915dd9cc3bdb74d
Oda Bastesen Storebo <599030@stud.hvl.no>
2023-04-12 11:40:39 +0200

    added, loginController and RegistrationController.

--- Uno/src/main/java/no/hvl/dat109/Uno/controllers/LoginController.java
+++ Uno/src/main/java/no/hvl/dat109/Uno/controllers/LoginController.java
@@ -1,20 +1,42 @@
 package no.hvl.dat109.Uno.controllers;
 
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import no.hvl.dat109.Uno.utils.DatabaseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @RestController
 @RequestMapping("/login")
 public class LoginController {
+
+    @Value("${}") private String INVALID_USERNAME_MESSAGE;
+    @Value("${}") private String LOGIN_URL;
+
     @Autowired
     DatabaseService db;
 
     @GetMapping
-    public String getRegisterView(){
+    public boolean getRegisterView(){
 
-        return "Hello World";
+        return true;
+    }
+
+    @PostMapping
+    public String logIn(@RequestParam String username, @RequestParam String pword, HttpServletRequest request, RedirectAttributes ra) {
+
+        if(username.equals("") || pword.equals("")) {
+            ra.addFlashAttriburte("redirectMessage", INVALID_USERNAME_MESSAGE);
+        }
+        return "redirect:" + LOGIN_URL;
+    }
+
+    DatabaseService user = db.findUser(name);
+
+    if(user == null) {
+        ra.addFlashAttribuite("redirectMessage", INVALID_USERNAME_MESSAGE);
+        return "redirect:" + LOGIN_URL;
     }
 }
